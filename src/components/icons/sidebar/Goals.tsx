9dc7932e0efc773de34b82152f46be61561788f7
mkowack <kowackag@gmail.com>
2024-10-04 23:47:48 +0200

    feat: add Logo icon, change Goal icon

--- src/components/icons/sidebar/Goals.tsx
+++ src/components/icons/sidebar/Goals.tsx
@@ -1,7 +1,7 @@
 export const Goals = ({ className }: { className?: string }) => {
   return (
     <svg
-      fill="#000000"
+      fill="currentColor"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
       width="28"
