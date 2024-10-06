2352939e892a1b945e93016d85c51471f1a167c7
mkowack <kowackag@gmail.com>
2024-10-06 14:46:04 +0200

    feat:add Topbar

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 
 import { Sidebar } from "./Sidebar/Sidebar";
+import { Topbar } from "./Topbar/Topbar";
 
 export const AppLayout = () => {
 	return (
 		<div className="flex h-full flex-col-reverse sm:flex-row">
 			<Sidebar />
 			<div className="flex-grow bg-secondary01/5">
-				{/* <Topbar/> */}
+				<Topbar />
 				<Outlet />
 			</div>
 		</div>
