2f4e994947afb357dd7dc2db3f9c2dbc73c04a70
Tomasz Rozpara <kowackag@gmail.com>
2024-10-18 19:09:40 +0200

    feat:getting income from firebase

--- src/pages/Income/Income.tsx
+++ src/pages/Income/Income.tsx
@@ -1,28 +1,29 @@
 import { useEffect, useState } from "react";
-import { IncomeType, incomeServices } from "../../services/income";
+import { IncomeType, incomeServices } from "@services/income";
 
 export const Income = () => {
-  const [allIncome, setAllIncome] = useState<IncomeType[]>([]);
-  const [error, setError] = useState<string | null>(null);
+	const [allIncome, setAllIncome] = useState<IncomeType[]>([]);
+	const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const getIncome = async () => {
-      const { isSuccess, income } = await incomeServices.getAll();
-      if (!isSuccess) setError("Some err during fetching data");
-      if (isSuccess) {
-        setAllIncome(income);
-      }
-    };
-    getIncome();
-  }, []);
+	useEffect(() => {
+		const getIncome = async () => {
+			try {
+				const income = await incomeServices.getAll();
+				setAllIncome(income);
+			} catch (e) {
+				setError("Some err during fetching data");
+			}
+		};
+		getIncome();
+	}, []);
 
-  if (error) return <p>{error}</p>;
-  return (
-    <section>
-      <h1>Income</h1>
-      {allIncome.map((el) => (
-        <p key={el.id}>{`${el.source}: ${el.value} ${el.currency}`}</p>
-      ))}
-    </section>
-  );
+	if (error) return <p>{error}</p>;
+	return (
+		<section>
+			<h1>Income</h1>
+			{allIncome.map((el) => (
+				<p key={el.id}>{`${el.source}: ${el.value} ${el.currency}`}</p>
+			))}
+		</section>
+	);
 };
--- src/services/income.ts
+++ src/services/income.ts
@@ -2,35 +2,38 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../config/firebase";
 
 export type IncomeType = {
-  id: string;
-  source: string;
-  value: number;
-  currency: string;
-  data: Date;
+	id: string;
+	source: string;
+	value: number;
+	currency: string;
+	data: Date;
+};
+const get = async () => {
+	throw new Error("Function not implemented.");
 };
 
-const getAll = async (): Promise<{
-  isSuccess: boolean;
-  income: IncomeType[];
-}> => {
-  try {
-    const incomeCollection = collection(db, "income");
-    const querySnapshot = await getDocs(incomeCollection);
-    const income = querySnapshot.docs.map((item) => {
-      return { ...item.data(), id: item.id };
-    });
-    return {
-      isSuccess: true,
-      income: income as IncomeType[],
-    };
-  } catch (error) {
-    return {
-      isSuccess: false,
-      income: [],
-    };
-  }
+const getAll = async (): Promise<IncomeType[]> => {
+	const incomeCollection = collection(db, "income");
+	const querySnapshot = await getDocs(incomeCollection);
+	return querySnapshot.docs.map(
+		(item) =>
+			({
+				...item.data(),
+				id: item.id,
+			}) as IncomeType,
+	);
 };
 
-const addIncome = async () => {};
+const add = async () => {
+	throw new Error("Function not implemented.");
+};
 
-export const incomeServices = { getAll, addIncome };
+const update = async () => {
+	throw new Error("Function not implemented.");
+};
+
+const remove = async () => {
+	throw new Error("Function not implemented.");
+};
+
+export const incomeServices = { get, getAll, add, update, remove };
