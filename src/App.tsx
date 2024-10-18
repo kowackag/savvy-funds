ccab0cf9afce72b99d90358e5e743b9d31a4c0d9
Tomasz Rozpara <kowackag@gmail.com>
2024-10-18 19:19:34 +0200

    refactor:remove React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import { appRoutes, authRoutes } from "./routes";
 import { AppLayout } from "./layouts/AppLayout/AppLayout";
