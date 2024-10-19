92ce545d2762e00f2ad3d58bd03055045b283b34
Tomasz Rozpara <kowackag@gmail.com>
2024-10-19 17:48:01 +0200

    feat:create Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,51 @@
+import clx from "classnames";
+import { ReactNode } from "react";
+
+export enum ButtonVariant {
+	Primary = "primary",
+	Secondary = "secondary",
+}
+
+export enum ButtonSize {
+	Small = "small",
+	Medium = "medium",
+	Large = "large",
+}
+
+type Props = {
+	variant: ButtonVariant;
+	className?: string;
+	children: ReactNode;
+	size?: ButtonSize;
+	type?: HTMLButtonElement["type"];
+	disabled?: boolean;
+};
+
+export const Button = ({
+	variant,
+	className,
+	children,
+	size,
+	type,
+	disabled=false,
+}: Props) => {
+	return (
+		<button
+			className={clx(
+				"align-center inline-flex justify-center gap-3 rounded-full",
+				className,
+				{
+					"bg-primary text-neutral00": variant === "primary",
+					"border-primary bg-neutral00 text-primary": variant === "secondary",
+					"py-3 px-6": size === "small",
+					"py-4 px-8": size === "medium",
+					"py-5 px-10": size === "large",
+				},
+			)}
+			type={type}
+			disabled={disabled}
+		>
+			{children}
+		</button>
+	);
+};
