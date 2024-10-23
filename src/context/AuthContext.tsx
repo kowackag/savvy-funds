49b006b79e3f0080e6e694b763d99cd47a26de6b
Tomasz Rozpara <kowackag@gmail.com>
2024-10-23 20:25:05 +0200

    feat: create AuthContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,50 @@
+import { User } from "firebase/auth";
+import { createContext, Dispatch, ReactNode, useReducer } from "react";
+
+type AuthAction =
+	| { type: "LOGIN"; payload: User }
+	| { type: "REGISTER"; payload: User }
+	| { type: "LOGOUT"; payload: null };
+
+type AuthContextValue = {
+	user: User | null;
+	dispatch: Dispatch<AuthAction>;
+};
+
+type AuthState = {
+	user: User | null;
+};
+
+export const authReducer = (state: AuthState, action: AuthAction) => {
+	switch (action.type) {
+		case "LOGIN":
+			return {
+				...state,
+				user: action.payload,
+			};
+		case "LOGOUT":
+			return {
+				...state,
+				user: null,
+			};
+		default:
+			return state;
+	}
+};
+
+export const AuthContext = createContext<AuthContextValue>({
+	user: null,
+	dispatch: () => undefined,
+});
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+	const [state, dispatch] = useReducer(authReducer, {
+		user: null,
+	});
+
+	return (
+		<AuthContext.Provider value={{ ...state, dispatch }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
