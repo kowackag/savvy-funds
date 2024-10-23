0f88702b849d8a0e1fccbfab465d249dd8cbd251
Tomasz Rozpara <kowackag@gmail.com>
2024-10-23 15:49:09 +0200

    fix:add lastname to register  schema

--- src/pages/Register/schema.ts
+++ src/pages/Register/schema.ts
@@ -4,12 +4,13 @@ export const schema = z.object({
 	firstName: z
 		.string()
 		.min(3, { message: "Name must contain at least 3 characters" }),
+	lastName: z.string(),
 	email: z.string().trim().email({ message: "Invalid email address" }),
 	password: z
 		.string()
 		.trim()
 		.min(8, { message: "Password must contain at least 8 characters" })
 		.regex(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/, {
-			message: "Password must contain both a letter and a number",
+			message: "Password must contain only and both a letter and a number",
 		}),
 });
