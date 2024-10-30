a20f78b5859054527610aec934beacde45667e07
Tomasz Rozpara <kowackag@gmail.com>
2024-10-30 07:51:28 +0100

    feat:add error tranlation

--- src/pages/Register/schema.ts
+++ src/pages/Register/schema.ts
@@ -1,16 +1,14 @@
 import * as z from "zod";
 
 export const schema = z.object({
-	firstName: z
-		.string()
-		.min(3, { message: "Name must contain at least 3 characters" }),
+	firstName: z.string().min(3, { message: "nameAtLeast3Characters" }),
 	lastName: z.string(),
 	email: z.string().trim().email({ message: "invalidEmail" }),
 	password: z
 		.string()
 		.trim()
-		.min(8, { message: "Password must contain at least 8 characters" })
+		.min(8, { message: "passwordAtLeast8Characters" })
 		.regex(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/, {
-			message: "Password must contain only and both a letter and a number",
+			message: "passwordOnlyBothLetterNumber",
 		}),
 });
