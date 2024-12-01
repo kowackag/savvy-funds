9613aec97eeb9e22799bcd2f90c058117231f3d0
Małgorzata Kowacka <kowackag@gmail.com>
2024-12-01 21:02:24 +0100

    feat: add icon Action, Edit, Trash

---
+++ src/components/icons/Actions.tsx
@@ -0,0 +1,18 @@
+export const ActionsIcon = ({ className }: { className?: string }) => {
+	return (
+		<svg
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			className={className}
+		>
+			<path
+				d="M11 6C11 5.44704 11.447 5 12 5C12.553 5 13 5.44704 13 6C13 6.55296 12.553 7 12 7C11.447 7 11 6.55296 11 6ZM11 12C11 11.447 11.447 11 12 11C12.553 11 13 11.447 13 12C13 12.553 12.553 13 12 13C11.447 13 11 12.553 11 12ZM11 18C11 17.447 11.447 17 12 17C12.553 17 13 17.447 13 18C13 18.553 12.553 19 12 19C11.447 19 11 18.553 11 18Z"
+				fill="currentColor"
+				stroke="currentColor"
+			/>
+		</svg>
+	);
+};
---
+++ src/components/icons/Trash.tsx
@@ -0,0 +1,17 @@
+export const TrashIcon = ({ className }: { className?: string }) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			className={className}
+		>
+			<path
+				d="M10.5 3C10.1074 3 9.70605 3.1377 9.42188 3.42188C9.1377 3.70605 9 4.10742 9 4.5V5.25H3.75V6.75H4.57031L6 20.3203L6.07031 21H17.9297L18 20.3203L19.4297 6.75H20.25V5.25H15V4.5C15 4.10742 14.8623 3.70605 14.5781 3.42188C14.2939 3.1377 13.8926 3 13.5 3H10.5ZM10.5 4.5H13.5V5.25H10.5V4.5ZM6.09375 6.75H17.9062L16.5703 19.5H7.42969L6.09375 6.75ZM9 9V17.25H10.5V9H9ZM11.25 9V17.25H12.75V9H11.25ZM13.5 9V17.25H15V9H13.5Z"
+				fill="currentColor"
+			/>
+		</svg>
+	);
+};
